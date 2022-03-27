86a754fb62da0dea0330ab964e5eda1ca8b7ca35
Adepentane <adepentane5@gmail.com>
2022-03-27 23:00:05 +0100

    Added whitenoise

--- smartnotes/settings.py
+++ smartnotes/settings.py
@@ -49,6 +49,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    heroku config:set DEBUG_COLLECTSTATIC=1
+'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'smartnotes.urls'
